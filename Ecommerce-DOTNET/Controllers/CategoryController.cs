e7a4518b599acaca63e19488810cb4a0a0c4649c
Mohammad Kashan Hussain <kashanhussainofficial89@gmail.com>
2026-06-23 15:12:22 +0500

    Create wala kaam done yaha tk!!

---
+++ Ecommerce-DOTNET/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using Ecommerce_DOTNET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_DOTNET.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ProjectDbContext context;
+
+        public IWebHostEnvironment Env { get; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public CategoryController(ProjectDbContext context, IWebHostEnvironment env)
+        {
+            this.context = context;
+            Env = env;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category, IFormFile Image)
+        {
+            string fileName = Guid.NewGuid().ToString() + "_" + Image.FileName;
+            string path = Path.Combine(Env.WebRootPath, "Images/", fileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                Image.CopyTo(stream);
+            }
+            category.Image = fileName;
+            this.context.Categories.Add(category);
+            this.context.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+
+    }
+}
