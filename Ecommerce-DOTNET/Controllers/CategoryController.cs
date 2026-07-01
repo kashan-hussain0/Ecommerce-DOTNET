595a7cb0156ecfb8275ec971dd86bbdb80b9dfda
Mohammad Kashan Hussain <kashanhussainofficial89@gmail.com>
2026-07-01 14:56:04 +0500

    Yha tk create delete read and update wala kaam done!

--- Ecommerce-DOTNET/Controllers/CategoryController.cs
+++ Ecommerce-DOTNET/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Ecommerce_DOTNET.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce_DOTNET.Controllers
 {
@@ -53,6 +54,31 @@ namespace Ecommerce_DOTNET.Controllers
             var modeldata = this.context.Categories.FirstOrDefault(x => x.Id == Id);
             this.context.Categories.Remove(modeldata);
             this.context.SaveChanges();
+            return RedirectToAction("Read");
+        }
+        //Update 
+        public  IActionResult Edit(int Id)
+        {
+            var modeldata = this.context.Categories.FirstOrDefault(x => x.Id == Id);
+            return View(modeldata);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category, IFormFile Image)
+        {
+            var modeldata = this.context.Categories.FirstOrDefault(x => x.Id == category.Id);
+            modeldata.CategoryName = category.CategoryName; 
+             if(Image != null)
+            {
+                string fileName = Guid.NewGuid().ToString() + "_" + Image.FileName;
+                string path = Path.Combine(Env.WebRootPath, "Images/", fileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    Image.CopyTo(stream);
+                }
+                modeldata.Image = fileName;
+            }
+             this.context.SaveChanges();
             return RedirectToAction("Read");
         }
     }
